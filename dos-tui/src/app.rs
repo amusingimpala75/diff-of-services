2d6431f9588a316b48d95e12dd271792da3e8696
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-05-14 21:02:16 -0700

    wrap and allow scrolling of diff

--- dos-tui/src/app.rs
+++ dos-tui/src/app.rs
@@ -6,7 +6,7 @@ use ratatui::{
     style::{Style, Stylize},
     symbols::border,
     text::Line,
-    widgets::{Block, List, Paragraph, Widget},
+    widgets::{Block, List, Paragraph, Widget, Wrap},
 };
 
 use anyhow::Result;
@@ -19,6 +19,7 @@ pub(crate) struct App {
     selected_revision_idx: usize,
     selected_revision: Option<Revision>,
     previous_revision: Option<Revision>,
+    vertical_scroll: u16,
 }
 
 impl App {
@@ -47,6 +48,7 @@ impl App {
     fn set_selected_revision(&mut self, idx: usize) -> Result<()> {
         let idx = idx.clamp(0, self.revisions.len());
         self.selected_revision_idx = idx;
+        self.vertical_scroll = 0;
         if self.selected_revision_idx < self.revisions.len() {
             self.selected_revision = Some(
                 self.revisions[self.selected_revision_idx]
@@ -71,21 +73,29 @@ impl App {
             Event::Key(key) => match key.code {
                 KeyCode::Char('q') => self.exit = true,
                 KeyCode::Up => {
-                    if key.modifiers.contains(KeyModifiers::SHIFT) {
+                    if key.modifiers.contains(KeyModifiers::ALT) {
                         if self.selected_document_idx > 0 {
                             self.set_selected_document(self.selected_document_idx - 1)?;
                         }
-                    } else if self.selected_revision_idx > 0 {
-                        self.set_selected_revision(self.selected_revision_idx - 1)?;
+                    } else if key.modifiers.contains(KeyModifiers::SHIFT) {
+                        if self.selected_revision_idx > 0 {
+                            self.set_selected_revision(self.selected_revision_idx - 1)?;
+                        }
+                    } else if self.vertical_scroll > 0 {
+                        self.vertical_scroll -= 1;
                     }
                 }
                 KeyCode::Down => {
-                    if key.modifiers.contains(KeyModifiers::SHIFT) {
+                    if key.modifiers.contains(KeyModifiers::ALT) {
                         if self.selected_document_idx + 1 < self.documents.len() {
                             self.set_selected_document(self.selected_document_idx + 1)?;
                         }
-                    } else if self.selected_revision_idx + 1 < self.revisions.len() {
-                        self.set_selected_revision(self.selected_revision_idx + 1)?;
+                    } else if key.modifiers.contains(KeyModifiers::SHIFT) {
+                        if self.selected_revision_idx + 1 < self.revisions.len() {
+                            self.set_selected_revision(self.selected_revision_idx + 1)?;
+                        }
+                    } else {
+                        self.vertical_scroll += 1;
                     }
                 }
                 _ => {}
@@ -98,23 +108,29 @@ impl App {
     pub(crate) fn default() -> Result<Self> {
         let conn = dos_lib::open_connection_file()?;
         let documents = Document::get_all(&conn)?;
-        let revisions = documents[0].revisions(&conn)?;
-        Ok(App {
+        let mut app = App {
             exit: false,
             documents,
             selected_document_idx: 0,
-            revisions,
+            revisions: vec![],
             selected_revision_idx: 0,
             selected_revision: None,
             previous_revision: None,
-        })
+            vertical_scroll: 0,
+        };
+
+        app.set_selected_document(0)?;
+
+        Ok(app)
     }
 }
 
 impl Widget for &App {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer) {
         let title = Line::from(" Diff of Services ");
-        let nav = Line::from(" S-Down / S-Up to change documents ");
+        let nav = Line::from(
+            " M-Down / M-Up to change documents, S-Up / S-Down to change revisions, Up / Down to scroll the document ",
+        );
 
         let generic_border = Block::bordered().border_set(border::THICK);
 
@@ -126,9 +142,9 @@ impl Widget for &App {
         (&main_border).render(area, buf);
 
         let [docu_select, rev_select, docu_display] = Layout::horizontal([
-            Constraint::Percentage(20),
-            Constraint::Percentage(20),
-            Constraint::Percentage(60),
+            Constraint::Percentage(15),
+            Constraint::Percentage(15),
+            Constraint::Percentage(70),
         ])
         .areas(main_border.inner(area));
 
@@ -174,16 +190,18 @@ impl Widget for &App {
                     diff::lines(&previous_text, &current_text)
                         .iter()
                         .map(|res| match res {
-                            diff::Result::Left(l) => Line::from(l.red()),
-                            diff::Result::Both(l, _) => Line::from(*l),
-                            diff::Result::Right(r) => Line::from(r.green()),
+                            diff::Result::Left(l) => Line::from(l.to_string().red()),
+                            diff::Result::Both(l, _) => Line::from(l.to_string()),
+                            diff::Result::Right(r) => Line::from(r.to_string().green()),
                         })
                         .collect::<Vec<_>>(),
                 )
-                .render(generic_border.inner(docu_display), buf);
             } else {
-                Paragraph::new(current_text).render(generic_border.inner(docu_display), buf);
+                Paragraph::new(current_text)
             }
+                .wrap(Wrap { trim: false })
+                .scroll((self.vertical_scroll * 5, 0))
+                .render(generic_border.inner(docu_display), buf);
         }
     }
 }
