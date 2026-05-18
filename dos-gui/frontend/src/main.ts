a3da5cfba98fe75cd8cca9baac72446f72521c25
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-05-18 14:49:04 -0700

    add inline and side by side diffing options to gui
    yes I know the css is pretty bad atm, I intend to fix that soon

--- dos-gui/frontend/src/main.ts
+++ dos-gui/frontend/src/main.ts
@@ -3,6 +3,7 @@ import { invoke } from "@tauri-apps/api/core";
 let documentSelect: HTMLSelectElement;
 let revisionSelect: HTMLSelectElement;
 let textElement: HTMLDivElement;
+let diffRadios: [HTMLInputElement];
 
 interface Revision {
   id: number,
@@ -15,14 +16,14 @@ interface Document {
 }
 
 async function loadDocuments() {
-  let documents: [Document] = await invoke("get_all_documents", {});
+  const documents: [Document] = await invoke("get_all_documents", {});
 
   while (documentSelect.children.length > 0) {
     documentSelect.remove(0);
   }
 
   for (const doc of documents) {
-    let option = document.createElement("option");
+    const option = document.createElement("option");
     option.value = doc.id.toString();
     option.label = doc.name;
     documentSelect.appendChild(option);
@@ -41,14 +42,14 @@ async function updateDocument() {
     return;
   }
 
-  let revisions: [Revision] = await invoke("get_document_revisions", {
-    id: parseInt(documentSelect.value),
+  const revisions: [Revision] = await invoke("get_document_revisions", {
+    id: parseInt(documentSelect.value, 10),
   });
 
   revisions.reverse();
 
   for (const rev of revisions) {
-    let option = document.createElement("option");
+    const option = document.createElement("option");
     option.value = rev.id.toString();
     option.label = rev.date_added.toString();
     revisionSelect.appendChild(option);
@@ -67,14 +68,131 @@ async function updateRevision() {
     return;
   }
 
+  for (const radio of diffRadios) {
+    if (radio.checked) {
+      switch (radio.value) {
+        case "none":
+          updateRevisionPlainText();
+          break;
+        case "inline":
+          updateRevisionInlineDiff();
+          break;
+        case "2col":
+          updateRevisionDiff2Col();
+          break;
+      }
+    }
+  }
+}
+
+async function updateRevisionPlainText() {
+
   const text: string = await invoke("get_revision_content", {
-    id: parseInt(revisionSelect.value),
+    id: parseInt(revisionSelect.value, 10),
   });
 
   for (const line of text.split("\n")) {
-    let p = document.createElement("p");
+    const p = document.createElement("p");
     p.textContent = line;
     textElement.appendChild(p);
+  }
+}
+
+enum DiffType {
+  ADD = "Add",
+  REMOVE = "Remove",
+  SAME = "Same"
+}
+
+interface DiffSegment {
+  text: string,
+  type: DiffType
+}
+
+async function updateRevisionInlineDiff() {
+  if (revisionSelect.selectedIndex + 1 === revisionSelect.options.length) {
+    updateRevisionPlainText();
+    return;
+  }
+  const current = revisionSelect.value;
+  const previous = revisionSelect.options[revisionSelect.selectedIndex + 1].value;
+
+  const diff: [[DiffSegment]] = await invoke("get_revision_diff", {
+    old: parseInt(previous, 10),
+    new: parseInt(current, 10),
+  });
+
+  for (const line of diff) {
+    const p = document.createElement("p");
+    for (const segment of line) {
+      const span = document.createElement("span");
+      span.textContent = segment.text;
+      console.log(segment.type);
+      switch (segment.type) {
+        case DiffType.ADD:
+          span.classList.add("diff-add");
+          break;
+        case DiffType.REMOVE:
+          span.classList.add("diff-remove");
+          break;
+        case DiffType.SAME:
+          span.classList.add("diff-same");
+          break;
+      }
+      p.appendChild(span);
+    }
+    textElement.appendChild(p);
+  }
+}
+
+async function updateRevisionDiff2Col() {
+  if (revisionSelect.selectedIndex + 1 === revisionSelect.options.length) {
+    updateRevisionPlainText();
+    return;
+  }
+  const current = revisionSelect.value;
+  const previous = revisionSelect.options[revisionSelect.selectedIndex + 1].value;
+
+  const diff: [[DiffSegment]] = await invoke("get_revision_diff", {
+    old: parseInt(previous, 10),
+    new: parseInt(current, 10),
+  });
+
+  const left = document.createElement("div");
+  const right = document.createElement("div");
+  textElement.appendChild(left);
+  textElement.appendChild(right);
+  left.classList.add("column");
+  right.classList.add("column");
+
+  for (const line of diff) {
+    const lp = document.createElement("p");
+    const rp = document.createElement("p");
+    left.appendChild(lp);
+    right.appendChild(rp);
+    for (const segment of line) {
+      const lspan = document.createElement("span");
+      const rspan = document.createElement("span");
+      lspan.textContent = segment.text;
+      rspan.textContent = segment.text;
+      lp.appendChild(lspan);
+      rp.appendChild(rspan);
+
+      switch (segment.type) {
+        case DiffType.ADD:
+          lspan.classList.add("hidden");
+          rspan.classList.add("diff-add")
+          break;
+        case DiffType.REMOVE:
+          lspan.classList.add("diff-remove");
+          rspan.classList.add("hidden");
+          break;
+        case DiffType.SAME:
+          lspan.classList.add("diff-same");
+          rspan.classList.add("diff-same");
+          break;
+      }
+    }
   }
 }
 
@@ -82,9 +200,13 @@ window.addEventListener("DOMContentLoaded", () => {
   documentSelect = document.querySelector("#documents")!;
   revisionSelect = document.querySelector("#revisions")!;
   textElement = document.querySelector("#document-revision-text")!;
+  diffRadios = <[HTMLInputElement]> Array.from(document.querySelector("#diff-type")!.children);
 
   documentSelect.addEventListener("change", updateDocument);
   revisionSelect.addEventListener("change", updateRevision);
+  diffRadios.map(radio => {
+    radio.addEventListener("change", updateRevision);
+  });
 
   loadDocuments();
 });
--- dos-gui/src/lib.rs
+++ dos-gui/src/lib.rs
@@ -1,4 +1,4 @@
-use dos_lib::{document::Document, revision::Revision};
+use dos_lib::{diff, document::Document, revision::Revision};
 
 #[derive(serde::Serialize)]
 struct DocumentStub {
@@ -93,6 +93,36 @@ fn get_revision_content(id: u32) -> Result<String, String> {
     Ok(text)
 }
 
+#[tauri::command]
+fn get_revision_diff(old: u32, new: u32) -> Result<Vec<Vec<diff::DiffSegment>>, String> {
+    let connection = match dos_lib::open_connection_file() {
+        Ok(conn) => conn,
+        Err(_) => return Err("could not open connection".to_string()),
+    };
+
+    let old_revision = match Revision::from_id(old, &connection) {
+        Ok(rev) => rev,
+        Err(_) => return Err(format!("could fetch revision {old}")),
+    };
+
+    let new_revision = match Revision::from_id(new, &connection) {
+        Ok(rev) => rev,
+        Err(_) => return Err(format!("could fetch revision {new}")),
+    };
+
+    let old_text = match old_revision.load_text(&connection) {
+        Ok(rev) => rev.text.unwrap(),
+        Err(_) => return Err(format!("Could not fetch text of revision {old}")),
+    };
+
+    let new_text = match new_revision.load_text(&connection) {
+        Ok(rev) => rev.text.unwrap(),
+        Err(_) => return Err(format!("Could not fetch text of revision {new}")),
+    };
+
+    Ok(diff::diff_lines_words(&old_text, &new_text))
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -109,7 +139,8 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![
             get_all_documents,
             get_document_revisions,
-            get_revision_content
+            get_revision_content,
+            get_revision_diff
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
